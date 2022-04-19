d91db2c99f0f9749a7ed5e733d5d6277512d6d66
YottaLee <849299796@qq.com>
2022-04-19 19:40:53 -0400

    ning: change hashtable to prefix tree

---
+++ tagvalueindex_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+
+	tree := NewTagValueIndex()
+
+	// add new tag value
+	tree.AddTagValue("intel", 0)
+	tree.AddTagValue("intel-i7", 1)
+	tree.AddTagValue("intel-i7", 2)
+	tree.AddTagValue("intel-i7", 5)
+	tree.AddTagValue("intel-i9", 4)
+	tree.AddTagValue("amd", 3)
+
+	// convert TagValueIndex to bytes
+	treeb := EncodeTagValueIndexToBytes(tree)
+
+	// convert bytes to TagValueIndex
+	treed := DecodeBytesToTagValueIndex(treeb)
+
+	fmt.Printf("%-18s %-8s %s\n", "prefix", "data", "error")
+	fmt.Printf("%-18s %-8s %s\n", "------", "----", "-----")
+
+	for _, prefix := range []string{
+		"a",
+		"a*",
+		"intel",
+		"inte*",
+		"intel-i7",
+		"intel-i9",
+	} {
+		data, err := treed.FindAllMatchedNodes(prefix)
+		fmt.Printf("%-18s %-8v %v\n", prefix, data, err)
+	}
+}
